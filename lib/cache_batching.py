081e5fb06737555bc19b621381a0c6ad92529d45
Seamus <seamusfallows1@gmail.com>
2024-01-21 11:44:22 +0000

    small refactor

--- lib/cache_batching.py
+++ lib/cache_batching.py
@@ -17,8 +17,9 @@ n_batch = 4
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
 
 # Load model and tokenizer
-model = AutoModelForCausalLM.from_pretrained("distilgpt2").to(device)
-tokenizer = AutoTokenizer.from_pretrained("distilgpt2", use_fast=True)
+MODEL_NAME = 'distilgpt2'
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME).to(device)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
 
 # Base prompt
 prompt = "She is "
@@ -29,24 +30,17 @@ outputs = model(input_ids, use_cache=True)
 cache = outputs.past_key_values
 
 # cache is a tuple of tuples of tensors, so need to loop through to repeat along bacth dimension of tensors.
-batched_cache = []
-for layer_cache in cache:
-    expanded_layer_cache = []
-    for tensor in layer_cache:
-        expanded_tensor = einops.repeat(tensor, 'batch ... -> (n batch) ...', n=n_batch)
-        expanded_layer_cache.append(expanded_tensor)
-    batched_cache.append(tuple(expanded_layer_cache))
-
-# Convert new_cache into a tuple of tuples
-batched_cache = tuple(batched_cache)
+batched_cache = tuple(
+    tuple(einops.repeat(tensor, 'batch ... -> (n batch) ...', n=n_batch) for tensor in layer_cache)
+    for layer_cache in cache
+)
 
 append_list = ['going', 'running', 'under', 'apple']
 
 # Tokens to append
-append_ids = t.tensor([[tokenizer.encode(append, add_special_tokens=False)] for append in append_list]).to(device)
+append_ids = t.tensor([tokenizer.encode(append, add_special_tokens=False) for append in append_list]).unsqueeze(1).to(device)
 
 # Perform a single forward pass with the batch of new tokens
-# Note: Repeat the cache for each new token in the batch
 outputs = model(append_ids, batched_cache)
 token_ids = outputs.logits[:,:,-1,:].squeeze().argmax(dim=-1)
 words = tokenizer.batch_decode(token_ids, skip_special_tokens=True)
