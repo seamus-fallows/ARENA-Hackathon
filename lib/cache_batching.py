c6c956fd839c2789050d59b97f2dfa2a1b5c37e1
Seamus <seamusfallows1@gmail.com>
2024-01-21 15:03:41 +0000

    experiment

--- lib/cache_batching.py
+++ lib/cache_batching.py
@@ -22,33 +22,31 @@ model = AutoModelForCausalLM.from_pretrained(MODEL_NAME).to(device)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
 
 # Base prompt
-prompt = "She is "
-input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
-
+prompt = ["She is ", "he is going to"]
+tokenizer.pad_token = tokenizer.eos_token
+# tokenizer.padding_side = 'left'
+input_ids = tokenizer(prompt, return_tensors="pt", padding=True)['input_ids'].to(device)
+print(input_ids)
 # Perform initial forward pass to generate cache
 outputs = model(input_ids, use_cache=True)
 cache = outputs.past_key_values
 
-# cache is a tuple of tuples of tensors, so need to loop through to repeat along bacth dimension of tensors.
-batched_cache = tuple(
-    tuple(einops.repeat(tensor, 'batch ... -> (n batch) ...', n=n_batch) for tensor in layer_cache)
-    for layer_cache in cache
-)
 
-append_list = ['going', 'running', 'under', 'apple']
+# # cache is a tuple of tuples of tensors, so need to loop through to repeat along bacth dimension of tensors.
+# batched_cache = tuple(
+#     tuple(einops.repeat(tensor, 'batch ... -> (n batch) ...', n=n_batch) for tensor in layer_cache)
+#     for layer_cache in cache
+# )
+
+append_list = ['going', 'running']
 
 # Tokens to append
 append_ids = t.tensor([tokenizer.encode(append, add_special_tokens=False) for append in append_list]).unsqueeze(1).to(device)
 
 # Perform a single forward pass with the batch of new tokens
-outputs = model(append_ids, batched_cache)
+outputs = model(append_ids, cache)
 token_ids = outputs.logits[:,:,-1,:].squeeze().argmax(dim=-1)
 words = tokenizer.batch_decode(token_ids, skip_special_tokens=True)
 print(words)
-
-
-
-
-
 
 # %%
